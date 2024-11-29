634de502adb0d9184f82b67d68256bb7fca9e709
SHOWVINI <id2046@naver.com>
2024-11-29 13:00:35 +0900

    ci: Update dev-ci.yml

--- test_application_connect_api.py
+++ test_application_connect_api.py
@@ -6,21 +6,6 @@ from application_connect_api import app
 # FastAPI TestClient 초기화
 client = TestClient(app)
 
-
-# 테스트 환경 준비: GitHub Actions Secrets에서 OPENAI_API_KEY 확인
-def test_environment_variables():
-    # GitHub Actions 환경 변수에서 OPENAI_API_KEY 가져오기
-    openai_api_key = os.getenv("OPENAI_API_KEY")
-    assert openai_api_key is not None, "OPENAI_API_KEY is not set in the environment variables."
-    assert openai_api_key.startswith("sk-"), "OPENAI_API_KEY does not seem to be a valid OpenAI API key."
-
-
-# 테스트 환경 준비: 프롬프트 파일의 존재 확인
-def test_prompt_config_file():
-    with open("image_to_text_description/prompt_config.yaml", "r") as f:
-        assert "prompts" in f.read()
-
-
 # 테스트: /ledger_receipt 엔드포인트
 def test_ledger_receipt_endpoint():
     # 상대 경로로 테스트에 사용할 파일 지정
